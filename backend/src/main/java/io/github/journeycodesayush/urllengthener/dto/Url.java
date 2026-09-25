7f45912972e666b06cf0bf98ca56fd74c50fc704
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-09-25 15:25:29 +0530

    feat(backend): add url request/response dto

---
+++ backend/src/main/java/io/github/journeycodesayush/urllengthener/dto/Url.java
@@ -0,0 +1,15 @@
+package io.github.journeycodesayush.urllengthener.dto;
+
+public class Url {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
