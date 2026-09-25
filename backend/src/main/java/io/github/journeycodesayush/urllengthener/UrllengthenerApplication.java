762ff98be632e00a9ed5aef0eec92f116a252798
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-09-25 13:30:19 +0530

    chore(backend): add initial project scaffold

---
+++ backend/src/main/java/io/github/journeycodesayush/urllengthener/UrllengthenerApplication.java
@@ -0,0 +1,13 @@
+package io.github.journeycodesayush.urllengthener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrllengthenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrllengthenerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/journeycodesayush/urllengthener/UrllengthenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.journeycodesayush.urllengthener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrllengthenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
