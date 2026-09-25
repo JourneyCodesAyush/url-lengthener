8cf41d9820a78ea5e2cb1c6f316fa1f3148b49a8
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-09-25 15:25:10 +0530

    feat(backend): add url entity

---
+++ backend/src/main/java/io/github/journeycodesayush/urllengthener/entity/UrlEntity.java
@@ -0,0 +1,48 @@
+package io.github.journeycodesayush.urllengthener.entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "urls")
+public class UrlEntity {
+
+    @Column(nullable = false, unique = true)
+    private String url;
+
+    @Id
+    @Column(nullable = false, unique = true, length = 64)
+    private String hashedUrl;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHashedUrl() {
+        return hashedUrl;
+    }
+
+    public void setHashedUrl(String hashedUrl) {
+        this.hashedUrl = hashedUrl;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
